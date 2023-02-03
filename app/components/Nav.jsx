25f0edc32a04ebacc64e680efe17a6818a1edd87
1infiniteloop <1infiniteloop.end@gmail.com>
2023-02-03 07:06:03 -0500

    .

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,13 +2,17 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
+function classNames(...classes) {
+	return classes.filter(Boolean).join(" ");
+}
+
 export default function Nav() {
 	return (
 		<Disclosure as="nav" className="bg-white shadow mb-[3px]">
 			{({ open }) => (
 				<>
-					<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-						<div className="flex h-10 justify-between">
+					<div className="mx-auto max-w-7xl px-[10px]">
+						<div className="flex h-12 justify-between">
 							<div className="flex">
 								<div className="flex flex-shrink-0 items-center">
 									<img
@@ -22,8 +26,7 @@ export default function Nav() {
 										alt="Your Company"
 									/>
 								</div>
-								<div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-									{/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+								{/* <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
 									<a
 										href="#"
 										className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
@@ -48,7 +51,7 @@ export default function Nav() {
 									>
 										Calendar
 									</a>
-								</div>
+								</div> */}
 							</div>
 							<div className="hidden sm:ml-6 sm:flex sm:items-center">
 								<button
@@ -138,7 +141,6 @@ export default function Nav() {
 								</Menu>
 							</div>
 							<div className="-mr-2 flex items-center sm:hidden">
-								{/* Mobile menu button */}
 								<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
 									<span className="sr-only">
 										Open main menu
