fbd6be1c7695a2b6dc2e9df540d3aa820936fe26
1infiniteloop <1infiniteloop.end@gmail.com>
2023-02-21 03:05:10 -0500

    .

--- app/components/CreditNav.jsx
+++ app/components/CreditNav.jsx
@@ -59,7 +59,7 @@ export default function Nav({ origin, can_share = false }) {
 									</div>
 									<Link
 										type="button"
-										className="text-blue-700  border border-blue-700  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800 flex flex-row"
+										className="text-blue-700 border border-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800 flex flex-row items-start justify-center"
 										to={share_link}
 									>
 										<svg
