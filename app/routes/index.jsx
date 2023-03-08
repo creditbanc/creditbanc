9cf7220abb9d66ee8bdaf9f4f67d9e8abea87099
1infiniteloop <1infiniteloop.end@gmail.com>
2023-03-07 20:50:33 -0500

    .

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -19,10 +19,10 @@ function classNames(...classes) {
 }
 
 const navigation = [
-	{ name: "Product", href: "#" },
-	{ name: "Features", href: "#" },
-	{ name: "Marketplace", href: "#" },
-	{ name: "Company", href: "#" },
+	{ name: "Small Business Lending", href: "#" },
+	{ name: "Tax Credit", href: "#" },
+	{ name: "Business Valuations", href: "#" },
+	{ name: "CB University", href: "#" },
 ];
 
 function Hero() {
@@ -1181,10 +1181,10 @@ function Stats() {
 
 const tiers = [
 	{
-		name: "Freelancer",
+		name: "Personal Credit Report",
 		id: "tier-freelancer",
 		href: "#",
-		priceMonthly: "$24",
+		priceMonthly: "$40",
 		description: "The essentials to provide your best work for clients.",
 		features: [
 			"5 products",
@@ -1195,10 +1195,10 @@ const tiers = [
 		mostPopular: false,
 	},
 	{
-		name: "Startup",
+		name: "Business & Personal Credit Report",
 		id: "tier-startup",
 		href: "#",
-		priceMonthly: "$32",
+		priceMonthly: "$50",
 		description: "A plan that scales with your rapidly growing business.",
 		features: [
 			"25 products",
@@ -1210,10 +1210,10 @@ const tiers = [
 		mostPopular: true,
 	},
 	{
-		name: "Enterprise",
+		name: "Business Credit Report",
 		id: "tier-enterprise",
 		href: "#",
-		priceMonthly: "$48",
+		priceMonthly: "$40",
 		description: "Dedicated support and infrastructure for your company.",
 		features: [
 			"Unlimited products",
@@ -1702,7 +1702,294 @@ function FAQ() {
 	);
 }
 
-export default function Index() {
+const footerNavigation = {
+	solutions: [
+		{ name: "Hosting", href: "#" },
+		{ name: "Data Services", href: "#" },
+		{ name: "Uptime Monitoring", href: "#" },
+		{ name: "Enterprise Services", href: "#" },
+	],
+	support: [
+		{ name: "Pricing", href: "#" },
+		{ name: "Documentation", href: "#" },
+		{ name: "Guides", href: "#" },
+		{ name: "API Reference", href: "#" },
+	],
+	company: [
+		{ name: "About", href: "#" },
+		{ name: "Blog", href: "#" },
+		{ name: "Jobs", href: "#" },
+		{ name: "Press", href: "#" },
+		{ name: "Partners", href: "#" },
+	],
+	legal: [
+		{ name: "Claim", href: "#" },
+		{ name: "Privacy", href: "#" },
+		{ name: "Terms", href: "#" },
+	],
+};
+
+export default function LandingPage() {
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+	return (
+		<div className="bg-white">
+			<header className="absolute inset-x-0 top-0 z-50">
+				<nav
+					className="flex items-center justify-between p-6 lg:px-8"
+					aria-label="Global"
+				>
+					<div className="flex lg:flex-1">
+						<a href="#" className="-m-1.5 p-1.5">
+							<span className="sr-only">Your Company</span>
+							<img
+								className="h-8 w-auto"
+								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								alt=""
+							/>
+						</a>
+					</div>
+					<div className="flex lg:hidden">
+						<button
+							type="button"
+							className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+							onClick={() => setMobileMenuOpen(true)}
+						>
+							<span className="sr-only">Open main menu</span>
+							<Bars3Icon className="h-6 w-6" aria-hidden="true" />
+						</button>
+					</div>
+					<div className="hidden lg:flex lg:gap-x-12">
+						{navigation.map((item) => (
+							<a
+								key={item.name}
+								href={item.href}
+								className="text-sm font-semibold leading-6 text-gray-900"
+							>
+								{item.name}
+							</a>
+						))}
+					</div>
+					<div className="hidden lg:flex lg:flex-1 lg:justify-end">
+						<a
+							href="#"
+							className="text-sm font-semibold leading-6 text-gray-900"
+						>
+							Log in <span aria-hidden="true">&rarr;</span>
+						</a>
+					</div>
+				</nav>
+				<Dialog
+					as="div"
+					className="lg:hidden"
+					open={mobileMenuOpen}
+					onClose={setMobileMenuOpen}
+				>
+					<div className="fixed inset-0 z-50" />
+					<Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+						<div className="flex items-center justify-between">
+							<a href="#" className="-m-1.5 p-1.5">
+								<span className="sr-only">Your Company</span>
+								<img
+									className="h-8 w-auto"
+									src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+									alt=""
+								/>
+							</a>
+							<button
+								type="button"
+								className="-m-2.5 rounded-md p-2.5 text-gray-700"
+								onClick={() => setMobileMenuOpen(false)}
+							>
+								<span className="sr-only">Close menu</span>
+								<XMarkIcon
+									className="h-6 w-6"
+									aria-hidden="true"
+								/>
+							</button>
+						</div>
+						<div className="mt-6 flow-root">
+							<div className="-my-6 divide-y divide-gray-500/10">
+								<div className="space-y-2 py-6">
+									{navigation.map((item) => (
+										<a
+											key={item.name}
+											href={item.href}
+											className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+										>
+											{item.name}
+										</a>
+									))}
+								</div>
+								<div className="py-6">
+									<a
+										href="#"
+										className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+									>
+										Log in
+									</a>
+								</div>
+							</div>
+						</div>
+					</Dialog.Panel>
+				</Dialog>
+			</header>
+			<main className="isolate">
+				<div className="relative pt-24">
+					<div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+						<svg
+							className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+							viewBox="0 0 1155 678"
+						>
+							<path
+								fill="url(#9b2541ea-d39d-499b-bd42-aeea3e93f5ff)"
+								fillOpacity=".3"
+								d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+							/>
+							<defs>
+								<linearGradient
+									id="9b2541ea-d39d-499b-bd42-aeea3e93f5ff"
+									x1="1155.49"
+									x2="-78.208"
+									y1=".177"
+									y2="474.645"
+									gradientUnits="userSpaceOnUse"
+								>
+									<stop stopColor="#9089FC" />
+									<stop offset={1} stopColor="#FF80B5" />
+								</linearGradient>
+							</defs>
+						</svg>
+					</div>
+					<div className="mx-auto max-w-7xl px-6 lg:px-8">
+						<div className="mx-auto max-w-2xl text-center">
+							<div className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+								<div className="py-2">Business & Personal</div>
+								<div>Credit Reports</div>
+							</div>
+							<p className="mt-6 text-lg leading-8 text-gray-600">
+								Anim aute id magna aliqua ad ad non deserunt
+								sunt. Qui irure qui lorem cupidatat commodo.
+								Elit sunt amet fugiat veniam occaecat fugiat
+								aliqua.
+							</p>
+						</div>
+					</div>
+					<div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+						<svg
+							className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+							viewBox="0 0 1155 678"
+						>
+							<path
+								fill="url(#b9e4a85f-ccd5-4151-8e84-ab55c66e5aa1)"
+								fillOpacity=".3"
+								d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+							/>
+							<defs>
+								<linearGradient
+									id="b9e4a85f-ccd5-4151-8e84-ab55c66e5aa1"
+									x1="1155.49"
+									x2="-78.208"
+									y1=".177"
+									y2="474.645"
+									gradientUnits="userSpaceOnUse"
+								>
+									<stop stopColor="#9089FC" />
+									<stop offset={1} stopColor="#FF80B5" />
+								</linearGradient>
+							</defs>
+						</svg>
+					</div>
+				</div>
+
+				<div className="mx-auto max-w-7xl px-6 lg:px-8">
+					<div className="isolate mx-auto mt-16 grid max-w-xl grid-cols-1 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+						{tiers.map((tier, tierIdx) => (
+							<div
+								key={tier.id}
+								className={classNames(
+									tier.mostPopular
+										? "lg:z-10 lg:rounded-b-none"
+										: "lg:mt-8",
+									tierIdx === 0 ? "lg:rounded-r-none" : "",
+									tierIdx === tiers.length - 1
+										? "lg:rounded-l-none"
+										: "",
+									"flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10"
+								)}
+							>
+								<div>
+									<div className="flex justify-between gap-x-4">
+										<h3
+											id={tier.id}
+											className={classNames(
+												tier.mostPopular
+													? "text-indigo-600"
+													: "text-gray-900",
+												"text-lg font-semibold"
+											)}
+										>
+											{tier.name}
+										</h3>
+										{tier.mostPopular ? (
+											<div className="w-[175px]">
+												<div className="rounded-full bg-indigo-600/10 text-xs font-semibold text-indigo-600 flex flex-row items-center justify-center mt-2 py-1">
+													Most popular
+												</div>
+											</div>
+										) : null}
+									</div>
+									<p className="mt-4 text-sm leading-6 text-gray-600">
+										{tier.description}
+									</p>
+									<p className="mt-6 flex items-baseline gap-x-1">
+										<span className="text-4xl font-bold tracking-tight text-gray-900">
+											{tier.priceMonthly}
+										</span>
+										<span className="text-sm font-semibold leading-6 text-gray-600">
+											/month
+										</span>
+									</p>
+									<ul
+										role="list"
+										className="mt-8 space-y-3 text-sm leading-6 text-gray-600"
+									>
+										{tier.features.map((feature) => (
+											<li
+												key={feature}
+												className="flex gap-x-3"
+											>
+												<CheckIcon
+													className="h-6 w-5 flex-none text-indigo-600"
+													aria-hidden="true"
+												/>
+												{feature}
+											</li>
+										))}
+									</ul>
+								</div>
+								<a
+									href={tier.href}
+									aria-describedby={tier.id}
+									className={classNames(
+										tier.mostPopular
+											? "bg-indigo-600 text-white shadow-sm hover:bg-indigo-500"
+											: "text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300",
+										"mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									)}
+								>
+									Pull Report
+								</a>
+							</div>
+						))}
+					</div>
+				</div>
+			</main>
+		</div>
+	);
+}
+
+function Index() {
 	return (
 		<div className="h-full w-full">
 			<main>
