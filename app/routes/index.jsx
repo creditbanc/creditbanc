bb4fcd2789d8c707a4d03f9df4f12bfa3b75be93
1infiniteloop <1infiniteloop.end@gmail.com>
2023-04-27 16:48:02 -0400

    .

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -281,6 +281,37 @@ function LogoCloud() {
 	);
 }
 
+const bureau_logos = [
+	"https://www.fico.com/sites/default/files/fico-logo-blue-large.png",
+	"https://vantagescore.com/wp-content/uploads/2022/03/vantagescore-logo.svg",
+	"https://www.dnb.com/content/dam/english/image-library/dnb-mod/logo-dnb.svg",
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Experian_logo.svg/1280px-Experian_logo.svg.png",
+	"https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/TransUnion_logo.svg/2560px-TransUnion_logo.svg.png",
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Equifax_Logo.svg/2560px-Equifax_Logo.svg.png",
+];
+
+function PoweredBy() {
+	return (
+		<div className="bg-white py-24 sm:py-32">
+			<div className="mx-auto max-w-7xl px-6 lg:px-8">
+				<h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+					Powered By
+				</h2>
+				<div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+					{bureau_logos.map((logo_src) => (
+						<img
+							className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+							src={logo_src}
+							width={158}
+							height={48}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
+
 const Single = () => {
 	return (
 		<div className="flex flex-col items-center">
@@ -526,7 +557,7 @@ export default function LandingPage() {
 					</div>
 				</div>
 				<div className="pb-14">
-					<LogoCloud />
+					<PoweredBy />
 				</div>
 				<div>
 					<FooterTwo />
