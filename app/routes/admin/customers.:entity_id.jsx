3c2a0ee14ce1b24b6e35eca2bdf11d9649351bd6
1infiniteloop <1infiniteloop.end@gmail.com>
2023-05-09 21:11:01 -0400

    .

--- app/routes/admin/customers.:entity_id.jsx
+++ app/routes/admin/customers.:entity_id.jsx
@@ -1,10 +1,17 @@
 import { pipe } from "ramda";
 import { get } from "shades";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useSubmit, useParams } from "@remix-run/react";
 import Nav from "~/components/CreditNav";
 import { inspect } from "~/utils/helpers";
-
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { get_user_id } from "~/utils/auth.server";
 var stripe = require("stripe");
+
+function classNames(...classes) {
+	return classes.filter(Boolean).join(" ");
+}
 
 export const loader = async ({ params }) => {
 	stripe = stripe(process.env.STRIPE);
@@ -26,6 +33,131 @@ export const loader = async ({ params }) => {
 	return { subscriptions };
 };
 
+export const action = async ({ request }) => {
+	console.log("update_subscription");
+	stripe = stripe(process.env.STRIPE);
+	let entity_id = await get_user_id(request);
+	var form = await request.formData();
+	const price_id = form.get("price_id");
+	const subscription_id = form.get("subscription_id");
+
+	const subscription = await stripe.subscriptions.retrieve(subscription_id);
+
+	stripe.subscriptions.update(subscription.id, {
+		cancel_at_period_end: false,
+		proration_behavior: "create_prorations",
+		items: [
+			{
+				id: subscription.items.data[0].id,
+				price: price_id,
+			},
+		],
+	});
+};
+
+function ActionsDropwdown({ subscription }) {
+	const submit = useSubmit();
+	const { entity_id } = useParams();
+	let { id: subscription_id } = subscription;
+
+	console.log("entity_id");
+	console.log(entity_id);
+
+	const onUpdateSubscription = (price_id) => {
+		console.log("onUpdateSubscription");
+
+		submit(
+			{ price_id, subscription_id },
+			{
+				method: "post",
+				action: `/admin/customers/${entity_id}`,
+			}
+		);
+	};
+
+	return (
+		<Menu as="div" className="relative inline-block text-left">
+			<div>
+				<Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+					Options
+					<ChevronDownIcon
+						className="-mr-1 h-5 w-5 text-gray-400"
+						aria-hidden="true"
+					/>
+				</Menu.Button>
+			</div>
+
+			<Transition
+				as={Fragment}
+				enter="transition ease-out duration-100"
+				enterFrom="transform opacity-0 scale-95"
+				enterTo="transform opacity-100 scale-100"
+				leave="transition ease-in duration-75"
+				leaveFrom="transform opacity-100 scale-100"
+				leaveTo="transform opacity-0 scale-95"
+			>
+				<Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+					<div className="py-1">
+						<Menu.Item>
+							{({ active }) => (
+								<a
+									href="#"
+									className={classNames(
+										active
+											? "bg-gray-100 text-gray-900"
+											: "text-gray-700",
+										"block px-4 py-2 text-sm"
+									)}
+								>
+									Update to Banc Essentials
+								</a>
+							)}
+						</Menu.Item>
+						<Menu.Item>
+							{({ active }) => (
+								<div
+									onClick={() =>
+										onUpdateSubscription(
+											"price_1N61DjJlRXkfyebsWDaUadR0"
+										)
+									}
+									className={classNames(
+										active
+											? "bg-gray-100 text-gray-900"
+											: "text-gray-700",
+										"block px-4 py-2 text-sm cursor-pointer"
+									)}
+								>
+									Update to Banc Builder
+								</div>
+							)}
+						</Menu.Item>
+						<Menu.Item>
+							{({ active }) => (
+								<a
+									onClick={() =>
+										onUpdateSubscription(
+											"price_1N5wxGJlRXkfyebs5BWgNLZU"
+										)
+									}
+									className={classNames(
+										active
+											? "bg-gray-100 text-gray-900"
+											: "text-gray-700",
+										"block px-4 py-2 text-sm cursor-pointer"
+									)}
+								>
+									Update to Banc Pro
+								</a>
+							)}
+						</Menu.Item>
+					</div>
+				</Menu.Items>
+			</Transition>
+		</Menu>
+	);
+}
+
 const SubscriptionCard = ({ subscription }) => {
 	return (
 		<div className="flex flex-col w-full">
@@ -40,7 +172,9 @@ const SubscriptionCard = ({ subscription }) => {
 						</div>
 					</div>
 				</div>
-				<div>Actions</div>
+				<div>
+					<ActionsDropwdown subscription={subscription} />
+				</div>
 			</div>
 
 			<div className="flex flex-col space-y-10">
@@ -256,7 +390,7 @@ export default function Customer() {
 			<div className="flex flex-col w-full border-b">
 				<Nav />
 			</div>
-			<div className="flex flex-col p-5">
+			<div className="flex flex-col p-8">
 				<SubscriptionCard subscription={subscriptions[0]} />
 			</div>
 		</div>
