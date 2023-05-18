cd05ea50261c62c42826e503d3e185cc0e430aef
1infiniteloop <1infiniteloop.end@gmail.com>
2023-05-18 07:12:47 -0400

    .

--- app/routes/credit/report/business.jsx
+++ app/routes/credit/report/business.jsx
@@ -22,6 +22,7 @@ import {
 	get_lendflow_report,
 } from "~/utils/lendflow.server";
 import deepEqual from "deep-equal";
+import UpgradeBanner from "~/components/UpgradeBanner";
 
 export const action = async ({ request }) => {
 	var form = await request.formData();
@@ -152,12 +153,12 @@ const UpgradeCard = () => {
 				<div className="sm:flex sm:items-start sm:justify-between">
 					<div>
 						<h3 className="text-base font-semibold leading-6 text-gray-900">
-							Upgrade to full report
+							Download the full report
 						</h3>
 						<div className="mt-2 max-w-xl text-sm text-gray-500">
 							<p>
-								Click to upgrade the report and to see the full
-								details.
+								Click to download the full report and to see all
+								the details.
 							</p>
 						</div>
 					</div>
@@ -167,7 +168,7 @@ const UpgradeCard = () => {
 							type="button"
 							className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 						>
-							Get Full Report
+							Download Report
 						</div>
 					</div>
 				</div>
@@ -201,6 +202,13 @@ export default function BusinessReport() {
 
 	return (
 		<div className="flex flex-col flex-1 overflow-scroll">
+			{plan_id == "essential" && (
+				<div className="flex flex-col w-full items-center">
+					<div className="flex flex-col w-full max-w-5xl">
+						<UpgradeBanner />
+					</div>
+				</div>
+			)}
 			{plans_index[report_plan_id] < plans_index[plan_id] && (
 				<div className="flex flex-col w-full items-center">
 					<div className="flex flex-col w-full max-w-5xl">
--- app/routes/home/$.jsx
+++ app/routes/home/$.jsx
@@ -17,6 +17,7 @@ import { Array, credit_report_data } from "~/data/array";
 import { useLoaderData } from "@remix-run/react";
 import { plans } from "~/data/plans";
 import { redirect } from "@remix-run/node";
+import UpgradeBanner from "~/components/UpgradeBanner";
 var cookie = require("cookie");
 
 export const loader = async ({ request }) => {
@@ -506,44 +507,14 @@ const HeadingTwo = () => {
 	);
 };
 
-const UpgradeCard = () => {
-	return (
-		<div className="bg-white border shadow sm:rounded-lg mx-2 my-5">
-			<div className="px-4 py-5 sm:p-6">
-				<div className="sm:flex sm:items-start sm:justify-between">
-					<div>
-						<h3 className="text-base font-semibold leading-6 text-gray-900">
-							Manage subscription
-						</h3>
-						<div className="mt-2 max-w-xl text-sm text-gray-500">
-							<p>
-								Unlock your full business credit reports and
-								scores to help you build credit faster and open
-								the door to better options for your business
-							</p>
-						</div>
-					</div>
-					<div className="mt-5 sm:ml-6 sm:mt-0 sm:flex sm:flex-shrink-0 sm:items-center">
-						<Link
-							to="/plans"
-							type="button"
-							className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-						>
-							Upgrade Plan
-						</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-};
-
 export default function Home() {
+	const { plan_id } = useLoaderData();
+
 	return (
 		<div className="w-full h-full flex flex-col items-center mb-3 p-5">
 			<HeadingTwo />
 			<div className="flex flex-col h-full max-w-7xl">
-				<UpgradeCard />
+				{plan_id == "essential" && <UpgradeBanner />}
 
 				<div className="flex flex-col lg:flex-row space-y-3 lg:space-y-0">
 					<div className="flex flex-col w-full">
