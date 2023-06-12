6d800bd01be0592aeefa3b75beb2487276f8a40e
1infiniteloop <1infiniteloop.end@gmail.com>
2023-06-12 15:42:01 -0400

    .

--- app/routes/vault/files.$.jsx
+++ app/routes/vault/files.$.jsx
@@ -1,3 +1,6 @@
+import { DocumentIcon } from "@heroicons/react/24/outline";
+import { sample } from "~/utils/helpers";
+
 const Heading = () => {
 	return (
 		<div className="border-b border-gray-200 pb-5">
@@ -22,8 +25,8 @@ const Heading = () => {
 
 const RecentTagsFilter = () => {
 	return (
-		<div className="flex flex-col w-full">
-			<div className="flex flex-row w-full items-center text-sm space-x-3 py-5">
+		<div className="flex flex-col w-full py-5">
+			<div className="flex flex-row w-full items-center text-sm space-x-3 ">
 				<div className="text-gray-400">Show</div>
 				<div className="flex flex-col px-3 py-1 border rounded-full text-gray-500 bg-gray-50 cursor-pointer">
 					Form 1040
@@ -45,6 +48,103 @@ const RecentTagsFilter = () => {
 	);
 };
 
+const FilesTableHeader = () => {
+	return (
+		<div className="flex flex-col w-full pt-5">
+			<div className="flex flex-row w-full text-sm text-gray-400 items-center border-b pb-5">
+				<div className="flex flex-col w-[40px]">
+					<input
+						type="checkbox"
+						className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 cursor-pointer"
+					/>
+				</div>
+				<div className="flex flex-col w-[250px]">Name</div>
+				<div className="flex flex-col flex-1">Tags</div>
+				<div className="flex flex-col w-[80px]">Year</div>
+				<div className="flex flex-col w-[80px]">Uploaded</div>
+			</div>
+		</div>
+	);
+};
+
+let category_styles = [
+	{
+		bg_color: "bg-red-100",
+		text_color: "text-red-500",
+		border_color: "border-red-500",
+	},
+	{
+		bg_color: "bg-blue-100",
+		text_color: "text-blue-500",
+		border_color: "border-blue-500",
+	},
+	{
+		bg_color: "bg-purple-100",
+		text_color: "text-purple-500",
+		border_color: "border-purple-500",
+	},
+	{
+		bg_color: "bg-green-100",
+		text_color: "text-green-500",
+		border_color: "border-green-500",
+	},
+	{
+		bg_color: "bg-orange-100",
+		text_color: "text-orange-500",
+		border_color: "border-orange-500",
+	},
+];
+
+let Category = ({ category }) => {
+	let { bg_color, text_color, border_color } = sample(category_styles);
+
+	return (
+		<div
+			className={`flex flex-col rounded-full w-[90px] overflow-hidden h-[25px] border -ml-[20px] first-of-type:ml-0 justify-center ${bg_color} ${text_color} ${border_color}`}
+		>
+			<div className={`flex flex-col w-[90%] px-2 overflow-hidden`}>
+				<div className={`w-[300px]`}>{category}</div>
+			</div>
+		</div>
+	);
+};
+
+const TableRow = () => {
+	return (
+		<div className="flex flex-row w-full border-b py-3 items-center text-sm">
+			<div className="flex flex-col w-[40px]">
+				<input
+					type="checkbox"
+					className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 cursor-pointer"
+				/>
+			</div>
+			<div className="flex flex-col w-[250px]">
+				<div className="flex flex-row items-center space-x-3">
+					<div className="flex flex-col ">
+						<DocumentIcon className="h-4 w-4 text-red-400" />
+					</div>
+					<div className="flex flex-col">1040</div>
+				</div>
+			</div>
+			<div className="flex flex-col flex-1">
+				<div className="flex flex-row w-full">
+					<Category category="Form 1040" />
+					<Category category="Form 1040" />
+					<Category category="Form 1040" />
+				</div>
+			</div>
+			<div className="flex flex-col w-[80px]">
+				<div className="flex flex-col px-3 py-1 border rounded-full text-gray-500 bg-gray-50 cursor-pointer items-center w-[60px]">
+					2022
+				</div>
+			</div>
+			<div className="flex flex-col w-[80px]">
+				<div>Dec 5, 2022</div>
+			</div>
+		</div>
+	);
+};
+
 export default function Files() {
 	return (
 		<div className="flex flex-col w-full h-full p-5">
@@ -53,6 +153,12 @@ export default function Files() {
 				<div className="flex flex-col flex-1 p-5">
 					<Heading />
 					<RecentTagsFilter />
+					<FilesTableHeader />
+					<div className="flex flex-col w-full">
+						<TableRow />
+						<TableRow />
+						<TableRow />
+					</div>
 				</div>
 			</div>
 		</div>
