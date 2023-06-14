be59b4d20ae0922297348a22d54d43c8339582b1
1infiniteloop <1infiniteloop.end@gmail.com>
2023-06-14 16:33:02 -0400

    .

--- app/routes/cashflow/accounts.$.jsx
+++ app/routes/cashflow/accounts.$.jsx
@@ -1,15 +1,54 @@
-import { currency } from "~/utils/helpers";
 import {
 	EllipsisHorizontalIcon,
-	PencilIcon,
-	ArrowDownTrayIcon,
-	TrashIcon,
 	EyeIcon,
 	LinkIcon,
+	PlusCircleIcon,
+	BuildingLibraryIcon,
 } from "@heroicons/react/24/outline";
 import { classNames } from "~/utils/helpers";
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
+import axios from "axios";
+import { usePlaidLink } from "react-plaid-link";
+import { create_axios_form, currency } from "~/utils/helpers";
+import { useEffect, useState } from "react";
+import { set_doc } from "~/utils/firebase";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ request }) => {
+	let location = new URL(request.url);
+	let { origin } = location;
+
+	let plaid_create_link_token_url = `${origin}/plaid/create_link_token`;
+
+	let config = {
+		method: "post",
+		maxBodyLength: Infinity,
+		url: plaid_create_link_token_url,
+	};
+
+	let response = await axios(config);
+
+	let { data } = response;
+	let { link_token } = data;
+
+	// let transactions = await sync_transactions();
+	// console.log("transactions");
+	// inspect(length(transactions));
+
+	// pipe(
+	// 	map(async (transaction) => {
+	// 		await set_doc(
+	// 			["transactions", transaction.transaction_id],
+	// 			transaction
+	// 		);
+	// 		console.log(`transaction ${transaction.transaction_id} saved`);
+	// 		return transaction;
+	// 	})
+	// )(transactions);
+
+	return { link_token };
+};
 
 const AccountActionsDropdown = ({ document }) => {
 	return (
@@ -97,10 +136,11 @@ const TableRow = ({ account }) => {
 		<div className="flex flex-row w-full text-gray-700 border-b py-3">
 			<div className="flex flex-row space-x-3 w-[250px]">
 				<div className="flex flex-col justify-center">
-					<span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-500">
-						<span className="font-medium leading-none text-white">
+					<span className="inline-flex h-10 w-10 items-center justify-center rounded-full ">
+						<BuildingLibraryIcon className="h-5 w-5 text-gray-400" />
+						{/* <span className="font-medium leading-none text-white">
 							TW
-						</span>
+						</span> */}
 					</span>
 				</div>
 				<div className="flex flex-col">
@@ -130,10 +170,46 @@ const TableRow = ({ account }) => {
 };
 
 export default function Accounts() {
+	const { link_token } = useLoaderData();
+	const [location, setLocation] = useState(null);
+
+	useEffect(() => setLocation(window.location), []);
+
+	const { open, ready } = usePlaidLink({
+		token: link_token,
+		onSuccess: async (public_token, metadata) => {
+			set_plaid(["public_token"], public_token);
+			let plaid_exchange_public_token_url = `${location.origin}/plaid/exchange_public_token`;
+			let data = create_axios_form({ public_token });
+
+			let config = {
+				method: "post",
+				maxBodyLength: Infinity,
+				headers: { "Content-Type": "multipart/form-data" },
+				url: plaid_exchange_public_token_url,
+				data,
+			};
+
+			let response = await axios(config);
+			let { access_token } = response.data;
+
+			if (access_token) {
+				console.log("plaid_exchange_public_token_response");
+				console.log(access_token);
+
+				let payload = {
+					access_token,
+				};
+
+				await set_doc(["bank_accounts", access_token], payload);
+			}
+		},
+	});
+
 	return (
 		<div className="flex flex-col w-full h-full p-5 overflow-hidden">
 			<div className="flex flex-col w-full h-full bg-white rounded p-5">
-				<div className="flex flex-row text-sm border-b pb-3">
+				<div className="flex flex-row text-sm border-b pb-3 text-gray-400">
 					<div className="flex flex-col w-[250px]">Account</div>
 					<div className="flex flex-col w-[200px]">
 						Account number
@@ -150,6 +226,15 @@ export default function Accounts() {
 					<TableRow />
 					<TableRow />
 					<TableRow />
+				</div>
+				<div
+					className="flex flex-row items-center space-x-5 border-b pb-3 cursor-pointer pl-1.5"
+					onClick={() => open()}
+				>
+					<div>
+						<PlusCircleIcon className="h-7 w-7 text-gray-400" />
+					</div>
+					<div>Link Account</div>
 				</div>
 			</div>
 		</div>
