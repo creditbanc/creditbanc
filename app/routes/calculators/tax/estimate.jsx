90d0a91d61a9296c629b304a290da0444cb1b089
1infiniteloop <1infiniteloop.end@gmail.com>
2023-06-16 12:33:02 -0400

    .

--- app/routes/calculators/tax/estimate.jsx
+++ app/routes/calculators/tax/estimate.jsx
@@ -50,7 +50,7 @@ export default function Estimate() {
 				>
 					<div className="flex lg:flex-1">
 						<Link to={"/"} className="-m-1.5 p-1.5">
-							<img className="h-8" src={cb_logo_3} alt="" />
+							<img className="h-6" src={cb_logo_3} alt="" />
 						</Link>
 					</div>
 				</nav>
--- app/routes/calculators/tax/industries.jsx
+++ app/routes/calculators/tax/industries.jsx
@@ -59,7 +59,7 @@ export default function Industries() {
 				>
 					<div className="flex lg:flex-1">
 						<Link to={"/"} className="-m-1.5 p-1.5">
-							<img className="h-8" src={cb_logo_3} alt="" />
+							<img className="h-6" src={cb_logo_3} alt="" />
 						</Link>
 					</div>
 				</nav>
