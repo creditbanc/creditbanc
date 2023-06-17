3eee312c18820790daf9f7a107e63e9bf98a5f98
1infiniteloop <1infiniteloop.end@gmail.com>
2023-06-17 17:55:14 -0400

    .

--- app/routes/companies/dashboard.jsx
+++ app/routes/companies/dashboard.jsx
@@ -21,24 +21,24 @@ const Account = ({ account }) => {
 			</div>
 			<div className="flex flex-col">
 				<div className="flex flex-row justify-between items-center">
-					<div class="flex -space-x-2 overflow-hidden">
+					<div className="flex -space-x-2 overflow-hidden">
 						<img
-							class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+							className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
 							src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 							alt=""
 						/>
 						<img
-							class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+							className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
 							src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 							alt=""
 						/>
 						<img
-							class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+							className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
 							src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
 							alt=""
 						/>
 						<img
-							class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+							className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
 							src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 							alt=""
 						/>
@@ -67,7 +67,7 @@ export default function Companies() {
 					<div></div>
 				</div>
 
-				<div className="flex flex-col w-full">
+				<div className="flex flex-col w-full mt-5">
 					<div className="flex flex-row w-full items-center flex-wrap gap-y-10 justify-between xl:justify-start xl:gap-x-5">
 						<Account />
 						<Account />
