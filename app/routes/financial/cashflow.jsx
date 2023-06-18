0ffd972be9cf7cf5b56b88a6870db89b32d76ada
1infiniteloop <1infiniteloop.end@gmail.com>
2023-06-18 10:30:15 -0400

    .

---
+++ app/routes/financial/cashflow.jsx
@@ -0,0 +1,26 @@
+export default function Cashflow() {
+	return (
+		<div className="flex flex-col w-full h-full overflow-hidden">
+			<div className="flex flex-row h-full w-full p-5 space-x-5">
+				<div className="flex flex-col h-full w-[70%] rounded  gap-y-5">
+					<div className="flex flex-col w-full min-h-[300px] bg-white rounded">
+						a
+					</div>
+					<div className="flex flex-row w-full gap-x-5 h-full">
+						<div className="flex flex-col w-1/2 bg-white h-full rounded">
+							a
+						</div>
+						<div className="flex flex-col w-1/2 bg-white h-full rounded">
+							a
+						</div>
+					</div>
+				</div>
+				<div className="flex flex-col w-[30%]  rounded">
+					<div className="flex flex-col w-full h-full rounded bg-white">
+						a
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
