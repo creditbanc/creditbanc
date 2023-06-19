dc0980719f499c62404eaf85305063af9895d9a2
1infiniteloop <1infiniteloop.end@gmail.com>
2023-06-19 18:11:55 -0400

    .

--- app/routes/financial/cashflow.jsx
+++ app/routes/financial/cashflow.jsx
@@ -478,8 +478,48 @@ const CashflowChart = () => {
 				Cashflow
 			</div>
 			<div className="flex flex-col w-full border-t my-3"></div>
-			<div className="flex flex-col w-full h-full p-3 overflow-hidden">
-				<Bar options={options} data={data} />
+			<div className="px-3">
+				<div className="flex flex-row w-full">
+					<div className="flex flex-row justify-between w-[30%]">
+						<div className="flex flex-col mb-7 space-y-2 my-2">
+							<div className="text-gray-700">
+								Net income in June
+							</div>
+							<div className="text-4xl">
+								{currency.format(5144707.08)}
+							</div>
+						</div>
+					</div>
+					<div className="flex flex-col flex-1 justify-end">
+						<div className="flex flex-row justify-center gap-x-5 py-6 text-sm">
+							<div className="flex flex-col hover:bg-gray-100 text-gray-600 px-3 rounded-full cursor-pointer">
+								30D
+							</div>
+							<div className="flex flex-col hover:bg-gray-100 text-gray-600 px-3 rounded-full cursor-pointer">
+								3M
+							</div>
+							<div className="flex flex-col hover:bg-gray-100 text-gray-600 px-3 rounded-full cursor-pointer">
+								6M
+							</div>
+							<div className="flex flex-col bg-blue-600 text-white px-3 rounded-full cursor-pointer">
+								12M
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="flex flex-row w-full">
+				<div className="flex flex-col w-[30%]">
+					<div className="pb-3 px-3">
+						<h3 className="text-base font-semibold leading-6 text-gray-900">
+							How you’re doing in June
+						</h3>
+					</div>
+					<IncomeStats />
+				</div>
+				<div className="flex flex-col flex-1 h-[350px] p-3 overflow-hidden">
+					<Bar options={options} data={data} />
+				</div>
 			</div>
 		</div>
 	);
@@ -501,6 +541,75 @@ const stats_data = [
 		changeType: "increase",
 	},
 ];
+
+const income_stats_data = [
+	{
+		name: "Month over month revenue change",
+		stat: "71,897",
+		previousStat: "70,946",
+		change: "12%",
+		changeType: "increase",
+	},
+	{
+		name: "Highest revenue in last 6 months",
+		stat: "58.16%",
+		previousStat: "56.14%",
+		change: "2.02%",
+		changeType: "increase",
+	},
+	{
+		name: "Highest revenue in last 6 months",
+		stat: "58.16%",
+		previousStat: "56.14%",
+		change: "2.02%",
+		changeType: "increase",
+	},
+];
+
+const IncomeStats = () => {
+	return (
+		<div className="bg-white divide-y rounded">
+			{income_stats_data.map((item) => (
+				<div key={item.name} className="py-5 px-3">
+					<div className="text-sm font-normal text-gray-900">
+						{item.name}
+					</div>
+					<div className="mt-1 flex items-baseline justify-between md:block lg:flex">
+						<div className="flex items-baseline text-xl font-semibold text-blue-600">
+							{item.stat}
+							<span className="ml-2 text-sm font-medium text-gray-500">
+								from {item.previousStat}
+							</span>
+						</div>
+
+						<div
+							className={classNames(
+								item.changeType === "increase"
+									? "bg-green-100 text-green-800"
+									: "bg-red-100 text-red-800",
+								"inline-flex items-baseline rounded-full px-2.5 py-0.5 text-xs font-medium md:mt-2 lg:mt-0"
+							)}
+						>
+							{item.changeType === "increase" ? (
+								<ArrowUpIcon
+									className="-ml-1 mr-0.5 h-4 w-4 flex-shrink-0 self-center text-green-500"
+									aria-hidden="true"
+								/>
+							) : (
+								<ArrowDownIcon
+									className="-ml-1 mr-0.5 h-4 w-4 flex-shrink-0 self-center text-red-500"
+									aria-hidden="true"
+								/>
+							)}
+
+							{item.change}
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
 
 const HealthStats = () => {
 	return (
@@ -568,7 +677,27 @@ const RevenueChart = () => {
 				<Bar options={options} data={revenue_data} />
 			</div>
 
+			<div className="flex flex-row justify-center gap-x-5 py-6 text-sm">
+				<div className="flex flex-col hover:bg-gray-100 text-gray-600 px-3 rounded-full cursor-pointer">
+					30D
+				</div>
+				<div className="flex flex-col hover:bg-gray-100 text-gray-600 px-3 rounded-full cursor-pointer">
+					3M
+				</div>
+				<div className="flex flex-col hover:bg-gray-100 text-gray-600 px-3 rounded-full cursor-pointer">
+					6M
+				</div>
+				<div className="flex flex-col bg-blue-600 text-white px-3 rounded-full cursor-pointer">
+					12M
+				</div>
+			</div>
+
 			<div>
+				<div className="border-b border-gray-200 pb-3 px-5">
+					<h3 className="text-base font-semibold leading-6 text-gray-900">
+						How you’re doing in June
+					</h3>
+				</div>
 				<HealthStats />
 			</div>
 		</div>
@@ -596,7 +725,27 @@ const ExpensesChart = () => {
 				<Bar options={options} data={expenses_data} />
 			</div>
 
+			<div className="flex flex-row justify-center gap-x-5 py-6 text-sm">
+				<div className="flex flex-col hover:bg-gray-100 text-gray-600 px-3 rounded-full cursor-pointer">
+					30D
+				</div>
+				<div className="flex flex-col hover:bg-gray-100 text-gray-600 px-3 rounded-full cursor-pointer">
+					3M
+				</div>
+				<div className="flex flex-col hover:bg-gray-100 text-gray-600 px-3 rounded-full cursor-pointer">
+					6M
+				</div>
+				<div className="flex flex-col bg-blue-600 text-white px-3 rounded-full cursor-pointer">
+					12M
+				</div>
+			</div>
+
 			<div>
+				<div className="border-b border-gray-200 pb-3 px-5">
+					<h3 className="text-base font-semibold leading-6 text-gray-900">
+						How you’re doing in June
+					</h3>
+				</div>
 				<HealthStats />
 			</div>
 		</div>
@@ -691,7 +840,7 @@ export default function Cashflow() {
 		<div className="flex flex-col w-full h-full overflow-hidden">
 			<div className="flex flex-row h-full w-full p-5 space-x-5 ">
 				<div className="flex flex-col h-full w-[70%] rounded  gap-y-5 overflow-scroll scrollbar-none">
-					<div className="flex flex-col w-full max-h-[400px] bg-white rounded">
+					<div className="flex flex-col w-full max-h-[600px] bg-white rounded">
 						<CashflowChart />
 					</div>
 					<div className="flex flex-row w-full gap-x-5 h-full">
