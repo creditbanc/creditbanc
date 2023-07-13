a265332aa2ab565d9bc35e5d82911ea752e23ecd
1infiniteloop <1infiniteloop.end@gmail.com>
2023-07-13 07:12:00 -0400

    .

---
+++ app/routes/credit/report/api.scores.$.jsx
@@ -0,0 +1,85 @@
+import { get_user_id } from "~/utils/auth.server";
+import { get_group_id } from "~/utils/helpers";
+import { defaultTo, head, isEmpty, omit, pipe } from "ramda";
+import { Lendflow } from "~/data/lendflow";
+import { Array } from "~/data/array";
+import axios from "axios";
+import { get_collection } from "~/utils/firebase";
+
+export const loader = async ({ request }) => {
+	let url = new URL(request.url);
+	let entity_id = await get_user_id(request);
+	let group_id = get_group_id(url.pathname);
+
+	let personal_credit_report_queries = [
+		{
+			param: "group_id",
+			predicate: "==",
+			value: group_id,
+		},
+		{
+			param: "type",
+			predicate: "==",
+			value: "personal_credit_report",
+		},
+	];
+
+	let personal_credit_report_response = await get_collection({
+		path: ["credit_reports"],
+		queries: personal_credit_report_queries,
+	});
+
+	let personal_credit_report = pipe(
+		head,
+		defaultTo({})
+	)(personal_credit_report_response);
+
+	let { data: personal = undefined } = personal_credit_report;
+
+	let personal_credit_report_payload = {
+		experian_personal_score: personal ? Array.experian.score(personal) : 0,
+		equifax_personal_score: personal ? Array.equifax.score(personal) : 0,
+		transunion_personal_score: personal
+			? Array.transunion.score(personal)
+			: 0,
+	};
+
+	let business_credit_report_queries = [
+		{
+			param: "group_id",
+			predicate: "==",
+			value: group_id,
+		},
+		{
+			param: "type",
+			predicate: "==",
+			value: "business_credit_report",
+		},
+	];
+
+	let business_credit_report_response = await get_collection({
+		path: ["credit_reports"],
+		queries: business_credit_report_queries,
+	});
+
+	let business_credit_report = pipe(
+		head,
+		defaultTo({})
+	)(business_credit_report_response);
+
+	let business_credit_report_payload = {
+		experian_business_score: business_credit_report
+			? Lendflow.experian.score(business_credit_report)
+			: 0,
+		dnb_business_score: business_credit_report
+			? Lendflow.dnb.score(business_credit_report)
+			: 0,
+	};
+
+	let payload = {
+		...personal_credit_report_payload,
+		...business_credit_report_payload,
+	};
+
+	return payload;
+};
--- app/routes/home/$.jsx
+++ app/routes/home/$.jsx
@@ -37,70 +37,12 @@ export const loader = async ({ request }) => {
 		},
 	});
 
-	let personal_credit_report_queries = [
-		{
-			param: "group_id",
-			predicate: "==",
-			value: group_id,
-		},
-		{
-			param: "type",
-			predicate: "==",
-			value: "personal_credit_report",
-		},
-	];
-
-	let personal_credit_report_response = await get_collection({
-		path: ["credit_reports"],
-		queries: personal_credit_report_queries,
+	let credit_scores_api_response = await axios({
+		method: "get",
+		url: `${url.origin}/credit/report/api/scores/resource/e/${entity_id}/g/${group_id}`,
 	});
 
-	let personal_credit_report = pipe(
-		head,
-		defaultTo({})
-	)(personal_credit_report_response);
-
-	let business_credit_report_queries = [
-		{
-			param: "group_id",
-			predicate: "==",
-			value: group_id,
-		},
-		{
-			param: "type",
-			predicate: "==",
-			value: "business_credit_report",
-		},
-	];
-
-	let business_credit_report_response = await get_collection({
-		path: ["credit_reports"],
-		queries: business_credit_report_queries,
-	});
-
-	let business_credit_report = pipe(
-		head,
-		defaultTo({})
-	)(business_credit_report_response);
-
-	let business_credit_report_payload = {
-		experian_business_score: business_credit_report
-			? Lendflow.experian.score(business_credit_report)
-			: 0,
-		dnb_business_score: business_credit_report
-			? Lendflow.dnb.score(business_credit_report)
-			: 0,
-	};
-
-	let { data: personal = undefined } = personal_credit_report;
-
-	let personal_credit_report_payload = {
-		experian_personal_score: personal ? Array.experian.score(personal) : 0,
-		equifax_personal_score: personal ? Array.equifax.score(personal) : 0,
-		transunion_personal_score: personal
-			? Array.transunion.score(personal)
-			: 0,
-	};
+	let { data: scores } = credit_scores_api_response;
 
 	let cashflow_api_response = await axios({
 		method: "get",
@@ -110,8 +52,7 @@ export const loader = async ({ request }) => {
 	let { data: financials } = cashflow_api_response;
 
 	let payload = {
-		...personal_credit_report_payload,
-		...business_credit_report_payload,
+		...scores,
 		entity_id,
 		plan_id,
 		financials,
