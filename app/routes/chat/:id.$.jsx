bb9378c45e23a77cacd445c7337ee4f32226d523
1infiniteloop <1infiniteloop.end@gmail.com>
2023-07-17 16:41:46 -0400

    .

--- app/routes/chat/:id.$.jsx
+++ app/routes/chat/:id.$.jsx
@@ -3,6 +3,7 @@ import {
 	ChevronRightIcon,
 	EllipsisHorizontalIcon,
 	FaceSmileIcon,
+	HashtagIcon,
 	MagnifyingGlassIcon,
 	PaperClipIcon,
 } from "@heroicons/react/24/outline";
@@ -518,7 +519,23 @@ export default function Chat() {
 			</div>
 			<div className="flex flex-row h-full overflow-hidden">
 				<div className="flex flex-col flex-1 justify-between bg-white">
-					<div className="flex flex-col bg-white px-5 overflow-hidden h-full">
+					<div className="flex flex-col bg-white px-5 overflow-hidden h-full relative">
+						<div className="flex flex-col h-full justify-end absolute bottom-[20px]">
+							<div className="flex flex-col gap-y-2">
+								<div className="flex flex-col bg-gray-400 rounded-full p-3 w-fit">
+									<HashtagIcon className="h-8 w-8 text-white" />
+								</div>
+								<div className="flex flex-col text-2xl font-semibold">
+									<div>Welcome to #{channel.title}!</div>
+								</div>
+								<div className="flex flex-col text-gray-600">
+									<div>
+										This is the start of the {channel.title}{" "}
+										channel.
+									</div>
+								</div>
+							</div>
+						</div>
 						<Messages />
 					</div>
 
