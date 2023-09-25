730ee8155a80078470f38d89daf75928ac0bca8c
1infiniteloop <1infiniteloop.end@gmail.com>
2023-09-25 11:57:29 -0400

    .

--- app/routes/role/:id.permissions.$.jsx
+++ app/routes/role/:id.permissions.$.jsx
@@ -1,34 +1,13 @@
 import { useEffect, useState } from "react";
 import { Switch } from "@headlessui/react";
-import {
-	classNames,
-	get_config_id,
-	get_group_id,
-	mapIndexed,
-	trim,
-	use_client_search_params,
-} from "~/utils/helpers";
+import { classNames, get_config_id, get_group_id, mapIndexed, trim, use_client_search_params } from "~/utils/helpers";
 import { pipe, map, isEmpty, filter, head } from "ramda";
 import { create } from "zustand";
 import { get, mod } from "shades";
 import { get_collection, get_doc, set_doc, update_doc } from "~/utils/firebase";
 import { useLoaderData, useLocation } from "@remix-run/react";
-import {
-	map as rxmap,
-	filter as rxfilter,
-	concatMap,
-	tap,
-	take,
-} from "rxjs/operators";
-import {
-	from,
-	lastValueFrom,
-	forkJoin,
-	Subject,
-	of as rxof,
-	merge,
-	throwError,
-} from "rxjs";
+import { map as rxmap, filter as rxfilter, concatMap, tap, take } from "rxjs/operators";
+import { from, lastValueFrom, forkJoin, Subject, of as rxof, merge, throwError } from "rxjs";
 import { fold, ifFalse } from "~/utils/operators";
 import { is_authorized_f } from "~/api/auth";
 import { get_session_entity_id } from "~/utils/auth.server";
@@ -52,11 +31,7 @@ const $loader = subject.pipe(
 
 		let redirect_home = entity_group_id.pipe(
 			concatMap(({ entity_id, group_id }) =>
-				throwError(() =>
-					Response.redirect(
-						`${url.origin}/home/resource/e/${entity_id}/g/${group_id}`
-					)
-				)
+				throwError(() => Response.redirect(`${url.origin}/home/resource/e/${entity_id}/g/${group_id}`))
 			)
 		);
 
@@ -80,9 +55,7 @@ const $loader = subject.pipe(
 			);
 
 		let is_authorized = entity_group_id.pipe(
-			concatMap(({ entity_id, group_id }) =>
-				is_authorized_f(entity_id, group_id, "share", "edit")
-			),
+			concatMap(({ entity_id, group_id }) => is_authorized_f(entity_id, group_id, "share", "edit")),
 			concatMap(ifFalse(redirect_home)),
 			concatMap(() => group_id),
 			concatMap(edit_roles),
@@ -108,8 +81,7 @@ const $loader = subject.pipe(
 
 export const usePermissionsStore = create((set) => ({
 	permissions: [],
-	set_permissions: (path, value) =>
-		set((state) => pipe(mod(...path)(() => value))(state)),
+	set_permissions: (path, value) => set((state) => pipe(mod(...path)(() => value))(state)),
 }));
 
 export const default_permissions = [
@@ -120,48 +92,48 @@ export const default_permissions = [
 		read: true,
 		edit: false,
 	},
-	{
-		id: "cashflow",
-		name: "Cashflow",
-		hidden: false,
-		read: true,
-		edit: false,
-	},
-	{
-		id: "accounts",
-		name: "Accounts",
-		hidden: false,
-		read: true,
-		edit: false,
-	},
-	{
-		id: "transactions",
-		name: "Transactions",
-		hidden: false,
-		read: true,
-		edit: false,
-	},
-	{
-		id: "vault",
-		name: "Vault",
-		hidden: false,
-		read: true,
-		edit: false,
-	},
-	{
-		id: "chat",
-		name: "Chat",
-		hidden: false,
-		read: true,
-		edit: false,
-	},
-	{
-		id: "share",
-		name: "Share",
-		hidden: false,
-		read: true,
-		edit: false,
-	},
+	// {
+	// 	id: "cashflow",
+	// 	name: "Cashflow",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: false,
+	// },
+	// {
+	// 	id: "accounts",
+	// 	name: "Accounts",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: false,
+	// },
+	// {
+	// 	id: "transactions",
+	// 	name: "Transactions",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: false,
+	// },
+	// {
+	// 	id: "vault",
+	// 	name: "Vault",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: false,
+	// },
+	// {
+	// 	id: "chat",
+	// 	name: "Chat",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: false,
+	// },
+	// {
+	// 	id: "share",
+	// 	name: "Share",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: false,
+	// },
 ];
 
 export const admin_permissions = [
@@ -172,48 +144,48 @@ export const admin_permissions = [
 		read: true,
 		edit: true,
 	},
-	{
-		id: "cashflow",
-		name: "Cashflow",
-		hidden: false,
-		read: true,
-		edit: true,
-	},
-	{
-		id: "accounts",
-		name: "Accounts",
-		hidden: false,
-		read: true,
-		edit: true,
-	},
-	{
-		id: "transactions",
-		name: "Transactions",
-		hidden: false,
-		read: true,
-		edit: true,
-	},
-	{
-		id: "vault",
-		name: "Vault",
-		hidden: false,
-		read: true,
-		edit: true,
-	},
-	{
-		id: "chat",
-		name: "Chat",
-		hidden: false,
-		read: true,
-		edit: true,
-	},
-	{
-		id: "share",
-		name: "Share",
-		hidden: false,
-		read: true,
-		edit: true,
-	},
+	// {
+	// 	id: "cashflow",
+	// 	name: "Cashflow",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: true,
+	// },
+	// {
+	// 	id: "accounts",
+	// 	name: "Accounts",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: true,
+	// },
+	// {
+	// 	id: "transactions",
+	// 	name: "Transactions",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: true,
+	// },
+	// {
+	// 	id: "vault",
+	// 	name: "Vault",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: true,
+	// },
+	// {
+	// 	id: "chat",
+	// 	name: "Chat",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: true,
+	// },
+	// {
+	// 	id: "share",
+	// 	name: "Share",
+	// 	hidden: false,
+	// 	read: true,
+	// 	edit: true,
+	// },
 ];
 
 export const loader = async ({ request }) => {
@@ -242,9 +214,7 @@ export const loader = async ({ request }) => {
 
 	subject.next({ id: "load", args: { request } });
 
-	let response = await lastValueFrom(
-		subject.pipe(rxfilter(on_complete), take(1))
-	);
+	let response = await lastValueFrom(subject.pipe(rxfilter(on_complete), take(1)));
 
 	return response.next();
 };
@@ -332,12 +302,8 @@ const SectionHeading = () => {
 	return (
 		<div className="flex flex-row justify-between items-end w-full border-b border-gray-200 pb-5">
 			<div>
-				<h3 className="text-base font-semibold leading-6 text-gray-900">
-					Pages Permissions
-				</h3>
-				<p className="mt-2 max-w-4xl text-sm text-gray-500">
-					Select what pages can be viewed or edited.
-				</p>
+				<h3 className="text-base font-semibold leading-6 text-gray-900">Pages Permissions</h3>
+				<p className="mt-2 max-w-4xl text-sm text-gray-500">Select what pages can be viewed or edited.</p>
 			</div>
 			{/* <div>
 				<div className="text-blue-600 cursor-pointer text-sm">
@@ -354,12 +320,8 @@ const RoleHeading = () => {
 	return (
 		<div className=" pb-2">
 			<div className="-ml-2 -mt-2 flex flex-wrap items-baseline">
-				<h3 className="ml-2 mt-2 text-base font-semibold leading-6 text-gray-900">
-					Role:
-				</h3>
-				<h3 className="ml-1 mt-1 truncate text-base text-gray-500">
-					{config?.name}
-				</h3>
+				<h3 className="ml-2 mt-2 text-base font-semibold leading-6 text-gray-900">Role:</h3>
+				<h3 className="ml-1 mt-1 truncate text-base text-gray-500">{config?.name}</h3>
 			</div>
 		</div>
 	);
@@ -404,11 +366,7 @@ export default function Permissions() {
 				<div className="flex flex-col w-full divide-y">
 					{pipe(
 						mapIndexed((permission, index) => (
-							<Permission
-								permission={permission}
-								index={index}
-								key={index}
-							/>
+							<Permission permission={permission} index={index} key={index} />
 						))
 					)(permissions)}
 				</div>
