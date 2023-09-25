8f5858e5022ff227d443d8c92f5205fcbfc6d8d1
1infiniteloop <1infiniteloop.end@gmail.com>
2023-09-25 10:55:27 -0400

    .

--- app/utils/lendflow.server.ts
+++ app/utils/lendflow.server.ts
@@ -430,8 +430,8 @@ export class LendflowExternal {
 				Authorization: `Bearer ${LENDFLOW_BEARER}`,
 				"Content-Type": "application/json",
 			},
-			data: { requested_products: ["experian_business_facts"] },
-			// data: { requested_products: LendflowExternal.plan_request_products("builder") },
+			// data: { requested_products: ["experian_business_facts"] },
+			data: { requested_products: LendflowExternal.plan_request_products("builder") },
 		};
 
 		let response = await axios(options);
