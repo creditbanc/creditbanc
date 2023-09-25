8a6dcd91fb3f2d0b70c09a8d7aa26cf520747d7a
1infiniteloop <1infiniteloop.end@gmail.com>
2023-09-25 12:53:25 -0400

    .

--- app/routes/_auth+/signin.jsx
+++ app/routes/_auth+/signin.jsx
@@ -48,23 +48,12 @@ export default function SignIn() {
 			<div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
 				<div className="mx-auto w-full max-w-sm lg:w-96">
 					<div>
-						<img
-							className="h-12 w-auto"
-							src={cb_logo}
-							alt="Your Company"
-						/>
-						<h2 className="my-2 mt-6 text-xl font-bold text-[#55CF9E]">
-							AREN’T YOU A SIGHT FOR SORE EYES
-						</h2>
-						<h2 className="my-3 text-3xl font-bold text-[#202536]">
-							Sign in below to access your account
-						</h2>
+						<img className="h-12 w-auto" src={cb_logo} alt="Your Company" />
+						<h2 className="my-2 mt-6 text-xl font-bold text-[#55CF9E]">AREN’T YOU A SIGHT FOR SORE EYES</h2>
+						<h2 className="my-3 text-3xl font-bold text-[#202536]">Sign in below to access your account</h2>
 						<p className="mt-2 text-sm text-gray-600 ">
 							First time here? Click{" "}
-							<a
-								href="/signup"
-								className="font-medium text-indigo-600 hover:text-indigo-500 underline"
-							>
+							<a href="/signup" className="font-medium text-indigo-600 hover:text-indigo-500 underline">
 								this link
 							</a>{" "}
 							to start your 14-day free trial!
@@ -73,16 +62,9 @@ export default function SignIn() {
 
 					<div className="mt-8">
 						<div className="mt-6">
-							<form
-								action="#"
-								method="POST"
-								className="space-y-6"
-							>
+							<form action="#" method="POST" className="space-y-6">
 								<div>
-									<label
-										htmlFor="email"
-										className="block text-sm font-medium text-gray-700"
-									>
+									<label htmlFor="email" className="block text-sm font-medium text-gray-700">
 										Email address
 									</label>
 									<div className="mt-1">
@@ -101,10 +83,7 @@ export default function SignIn() {
 								</div>
 
 								<div className="space-y-1">
-									<label
-										htmlFor="password"
-										className="block text-sm font-medium text-gray-700"
-									>
+									<label htmlFor="password" className="block text-sm font-medium text-gray-700">
 										Password
 									</label>
 									<div className="mt-1">
@@ -130,22 +109,19 @@ export default function SignIn() {
 											type="checkbox"
 											className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
 										/>
-										<label
-											htmlFor="remember-me"
-											className="ml-2 block text-sm text-gray-900"
-										>
+										<label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
 											Remember me
 										</label>
 									</div>
 
-									<div className="text-sm">
+									{/* <div className="text-sm">
 										<a
 											href="#"
 											className="font-medium text-indigo-600 hover:text-indigo-500 underline"
 										>
 											Help! I can’t remember my password!
 										</a>
-									</div>
+									</div> */}
 								</div>
 
 								<div>
@@ -162,10 +138,7 @@ export default function SignIn() {
 				</div>
 			</div>
 			<div className="relative hidden w-0 flex-1 lg:block">
-				<img
-					className="absolute inset-0 h-full w-full object-cover"
-					src={bg}
-				/>
+				<img className="absolute inset-0 h-full w-full object-cover" src={bg} />
 			</div>
 		</div>
 	);
