14b5653a42dd1954ba02b3aa99a6ce6ef534bf7c
1infiniteloop <1infiniteloop.end@gmail.com>
2023-10-03 03:19:12 -0400

    .

--- app/routes/credit/report/personal/personal/$.jsx
+++ app/routes/credit/report/personal/personal/$.jsx
@@ -8,6 +8,7 @@ import { on_success } from "~/routes/credit/report/personal/success";
 import { is_authorized } from "../authorized";
 import { redirect } from "@remix-run/node";
 import { appKey } from "~/data/array";
+import moment from "moment";
 
 const log_route = `credit.report.personal.personal`;
 
@@ -22,23 +23,25 @@ export const loader = async ({ request }) => {
 	let url = new URL(request.url);
 	let group_id = get_group_id(url.pathname);
 	let report = new PersonalReport(group_id);
-	let payload = report.user_token.fold;
+	let payload = report.user_token.first_name.last_name.fold;
 	let response = await lastValueFrom(payload.pipe(fold(on_success(request), on_error)));
 	return response;
 };
 
 export default function Personal() {
 	let loader_data = useLoaderData();
-	let { user_token } = loader_data;
+	let { user_token, first_name, last_name } = loader_data;
 
 	return (
 		<div>
 			<div className="flex flex-col w-full items-center my-10 px-5">
 				<div className="flex flex-col max-w-4xl text-center">
-					<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Matthew Meehan</h1>
+					<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+						{first_name} {last_name}
+					</h1>
 					<div className="flex flex-row w-full justify-center gap-x-2 mt-6 text-lg leading-8 text-gray-600">
 						<div>Personal:</div>
-						<div className="font-semibold">09-29-2023</div>
+						<div className="font-semibold">{moment().format("MM-DD-YYYY")}</div>
 					</div>
 				</div>
 			</div>
