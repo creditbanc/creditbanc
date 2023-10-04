11d5610af1561289616900beebab081128ebdc81
1infiniteloop <1infiniteloop.end@gmail.com>
2023-10-03 23:01:20 -0400

    .

--- app/routes/credit/business/new/v.1.$.jsx
+++ app/routes/credit/business/new/v.1.$.jsx
@@ -12,6 +12,11 @@ import { from, of as rxof, Subject, lastValueFrom, catchError } from "rxjs";
 import Entity from "~/api/client/Entity";
 import { fold } from "~/utils/operators";
 import axios from "axios";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/24/outline";
+const welcome_img = "/images/Welcome.png";
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from "@heroicons/react/20/solid";
 
 let route_logger = `credit.business.new`;
 let action_start = "credit.business.new.action.start";
@@ -105,6 +110,114 @@ export const action = async ({ request }) => {
 	return response.next();
 };
 
+const features = [
+	{
+		name: "Push to deploy.",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+		icon: CloudArrowUpIcon,
+	},
+	{
+		name: "SSL certificates.",
+		description: "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+		icon: LockClosedIcon,
+	},
+	{
+		name: "Database backups.",
+		description: "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+		icon: ServerIcon,
+	},
+];
+
+const Features = () => {
+	return (
+		<div className="overflow-hidden bg-white">
+			<div className="mx-auto max-w-7xl px-6 lg:px-8">
+				<div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+					<div className="lg:pr-8 lg:pt-4">
+						<div className="lg:max-w-lg">
+							<h2 className="text-base font-semibold leading-7 text-green-400">Deploy faster</h2>
+							<p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+								A better workflow
+							</p>
+							<p className="mt-6 text-lg leading-8 text-gray-600">
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
+								suscipit eaque, iste dolor cupiditate blanditiis ratione.
+							</p>
+							<dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+								{features.map((feature) => (
+									<div key={feature.name} className="relative pl-9">
+										<dt className="inline font-semibold text-gray-900">
+											<feature.icon
+												className="absolute left-1 top-1 h-5 w-5 text-green-400"
+												aria-hidden="true"
+											/>
+											{feature.name}
+										</dt>{" "}
+										<dd className="inline">{feature.description}</dd>
+									</div>
+								))}
+							</dl>
+							<div className="flex flex-col w-full px-3 py-2 bg-green-400 rounded text-white items-center my-5 cursor-pointer">
+								Get started
+							</div>
+						</div>
+					</div>
+					<div className="flex flex-col w-full h-full justify-center">
+						<img
+							src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+							alt="Product screenshot"
+							className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+							width={2432}
+							height={1442}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const WelcomeModal = () => {
+	const [open, setOpen] = useState(true);
+
+	return (
+		<Transition.Root show={open} as={Fragment}>
+			<Dialog as="div" className="relative z-10" onClose={setOpen}>
+				<Transition.Child
+					as={Fragment}
+					enter="ease-out duration-300"
+					enterFrom="opacity-0"
+					enterTo="opacity-100"
+					leave="ease-in duration-200"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
+				>
+					<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+				</Transition.Child>
+
+				<div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+					<div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+						<Transition.Child
+							as={Fragment}
+							enter="ease-out duration-300"
+							enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+							enterTo="opacity-100 translate-y-0 sm:scale-100"
+							leave="ease-in duration-200"
+							leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+							leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+						>
+							<Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white py-4 text-left shadow-xl transition-all">
+								<Features />
+							</Dialog.Panel>
+						</Transition.Child>
+					</div>
+				</div>
+			</Dialog>
+		</Transition.Root>
+	);
+};
+
 export default function View() {
 	let { pathname } = useLocation();
 	let form = use_form_store((state) => state.form);
@@ -137,16 +250,13 @@ export default function View() {
 	};
 
 	return (
-		<div className="flex flex-col w-full overflow-y-scroll scrollbar-none items-center">
+		<div className="flex flex-col w-full h-full overflow-y-scroll scrollbar-none items-center justify-center">
+			<WelcomeModal />
 			<div className="flex flex-col w-[900px]">
-				<div className="bg-white mt-4">
-					<div className="mx-auto max-w-2xl text-center">
-						<h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-							Business Credit Report
-						</h2>
+				<div className="flex flex-col border rounded">
+					<div className="flex flex-col w-full border-b py-3 px-5">
+						<div className="font-bold tracking-tight text-gray-900">Business Credit Report</div>
 					</div>
-				</div>
-				<div className="flex flex-col border rounded my-6">
 					<NewBusinessReportForm on_submit={onSubmit} />
 				</div>
 			</div>
