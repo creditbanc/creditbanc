b32defe8a60b252496b04ddb526ad5871d84ef0c
1infiniteloop <1infiniteloop.end@gmail.com>
2023-11-10 11:43:44 -0500

    .

--- app/routes/university/course.curriculum.$.jsx
+++ app/routes/university/course.curriculum.$.jsx
@@ -10,12 +10,12 @@ const UniversityHeader = () => {
 	return (
 		<div className="px-5 mb-5">
 			<div className="mx-auto max-w-2xl lg:mx-0">
-				<h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Portfolio Manager</h2>
-				<p className="mt-6 text-lg leading-8 text-gray-600">
+				<h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Credit Builder</h2>
+				{/* <p className="mt-6 text-lg leading-8 text-gray-600">
 					Gain instant access to a library of online finance courses utilized by top global banks and
 					financial institutions. Develop a comprehensive understanding of practical skills to enhance your
 					career or excel in interviews, whether you’re an intern, analyst or associate.
-				</p>
+				</p> */}
 			</div>
 		</div>
 	);
@@ -44,12 +44,17 @@ const CurriculumAccordion = ({ sections = [] }) => {
 									{pipe(
 										map((resource) => (
 											<Link
-												to={`/university/course/resource/resource/e/${entity_id}/g/${group_id}/f/${resource.id}`}
+												to={`${resource.href}/resource/e/${entity_id}/g/${group_id}/f/${resource.id}`}
 												className="flex flex-row w-full border p-2 rounded"
 											>
 												<div className="flex flex-row w-full space-x-2 items-center space-between cursor-pointer">
 													<div>
-														<PlayCircleIcon className="h-5 w-5 text-gray-500" />
+														{resource.type == "video" && (
+															<PlayCircleIcon className="h-5 w-5 text-gray-500" />
+														)}
+														{resource.type !== "video" && (
+															<DocumentIcon className="h-4 w-4 text-gray-500" />
+														)}
 													</div>
 													<div>{resource?.title}</div>
 												</div>
@@ -68,6 +73,10 @@ const CurriculumAccordion = ({ sections = [] }) => {
 };
 
 export default function Courses() {
+	let { pathname } = useLocation();
+	let entity_id = get_entity_id(pathname);
+	let group_id = get_group_id(pathname);
+
 	return (
 		<div className="flex flex-row w-full h-full overflow-hiddens gap-x-5 overflow-hidden">
 			<div className="flex flex-col w-full lg:w-[70%] h-full rounded overflow-scroll scrollbar-none">
@@ -85,7 +94,7 @@ export default function Courses() {
 						/>
 					</div>
 
-					<div className="border-gray-200 bg-white px-6 border-b pb-5">
+					{/* <div className="border-gray-200 bg-white px-6 border-b pb-5">
 						<div className="flex flex-col w-full">
 							<div className="flex flex-row w-full justify-between my-2 text-sm text-gray-400">
 								<div>45%</div>
@@ -95,14 +104,14 @@ export default function Courses() {
 								<div className="bg-blue-600 h-2.5 rounded-full w-[45%]"></div>
 							</div>
 						</div>
-					</div>
+					</div> */}
 
 					<div className="flex flex-col w-full h-[90px] px-5 justify-center">
 						<Link
-							to={`/university/course/1`}
+							to={`/university/creditbuilder/1/resource/e/${entity_id}/g/${group_id}/f/1`}
 							className="flex flex-col w-full py-4 items-center justify-center bg-blue-600 text-white rounded cursor-pointer"
 						>
-							Resume
+							Start
 						</Link>
 					</div>
 				</div>
--- app/routes/university/courses.$.jsx
+++ app/routes/university/courses.$.jsx
@@ -89,80 +89,78 @@ const HeaderFilters = () => {
 const posts = [
 	{
 		id: 1,
-		title: "Boost your conversion rate",
-		href: "#",
-		description:
-			"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+		title: "Credit Builder",
+		href: `/university/creditbuilder/1`,
+		description: "",
 		imageUrl:
 			"https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
-		date: "Mar 16, 2020",
-		datetime: "2020-03-16",
-		category: { title: "Marketing", href: "#" },
+		date: "Nov 1, 2023",
+		datetime: "2023-11-01",
+		category: { title: "Credit", href: "#" },
 		author: {
-			name: "Michael Foster",
-			role: "Co-Founder / CTO",
-			href: "#",
-			imageUrl:
-				"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+			name: "Credit Banc University",
+			// role: "Co-Founder / CTO",
+			// href: "#",
+			// imageUrl: "http://creditbanc.io/images/logos/cb_logo_3.png",
 		},
 	},
-	{
-		id: 1,
-		title: "Boost your conversion rate",
-		href: "#",
-		description:
-			"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
-		date: "Mar 16, 2020",
-		datetime: "2020-03-16",
-		category: { title: "Marketing", href: "#" },
-		author: {
-			name: "Michael Foster",
-			role: "Co-Founder / CTO",
-			href: "#",
-			imageUrl:
-				"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-		},
-	},
-	{
-		id: 1,
-		title: "Boost your conversion rate",
-		href: "#",
-		description:
-			"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
-		date: "Mar 16, 2020",
-		datetime: "2020-03-16",
-		category: { title: "Marketing", href: "#" },
-		author: {
-			name: "Michael Foster",
-			role: "Co-Founder / CTO",
-			href: "#",
-			imageUrl:
-				"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-		},
-	},
-	{
-		id: 1,
-		title: "Boost your conversion rate",
-		href: "#",
-		description:
-			"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
-		date: "Mar 16, 2020",
-		datetime: "2020-03-16",
-		category: { title: "Marketing", href: "#" },
-		author: {
-			name: "Michael Foster",
-			role: "Co-Founder / CTO",
-			href: "#",
-			imageUrl:
-				"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-		},
-	},
+	// {
+	// 	id: 1,
+	// 	title: "Boost your conversion rate",
+	// 	href: "#",
+	// 	description:
+	// 		"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+	// 	imageUrl:
+	// 		"https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
+	// 	date: "Mar 16, 2020",
+	// 	datetime: "2020-03-16",
+	// 	category: { title: "Marketing", href: "#" },
+	// 	author: {
+	// 		name: "Michael Foster",
+	// 		role: "Co-Founder / CTO",
+	// 		href: "#",
+	// 		imageUrl:
+	// 			"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+	// 	},
+	// },
+	// {
+	// 	id: 1,
+	// 	title: "Boost your conversion rate",
+	// 	href: "#",
+	// 	description:
+	// 		"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+	// 	imageUrl:
+	// 		"https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
+	// 	date: "Mar 16, 2020",
+	// 	datetime: "2020-03-16",
+	// 	category: { title: "Marketing", href: "#" },
+	// 	author: {
+	// 		name: "Michael Foster",
+	// 		role: "Co-Founder / CTO",
+	// 		href: "#",
+	// 		imageUrl:
+	// 			"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+	// 	},
+	// },
+	// {
+	// 	id: 1,
+	// 	title: "Boost your conversion rate",
+	// 	href: "#",
+	// 	description:
+	// 		"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+	// 	imageUrl:
+	// 		"https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+	// 	date: "Mar 16, 2020",
+	// 	datetime: "2020-03-16",
+	// 	category: { title: "Marketing", href: "#" },
+	// 	author: {
+	// 		name: "Michael Foster",
+	// 		role: "Co-Founder / CTO",
+	// 		href: "#",
+	// 		imageUrl:
+	// 			"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+	// 	},
+	// },
 ];
 
 const Accounts = () => {
@@ -215,7 +213,7 @@ const Accounts = () => {
 							<p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
 						</div>
 
-						<div className="my-2 flex flex-col w-full">
+						{/* <div className="my-2 flex flex-col w-full">
 							<div className="flex flex-row w-full justify-between my-2 text-sm text-gray-400">
 								<div>45%</div>
 								<div>4/20 lessons</div>
@@ -223,10 +221,10 @@ const Accounts = () => {
 							<div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
 								<div className="bg-blue-600 h-2.5 rounded-full w-[45%]"></div>
 							</div>
-						</div>
+						</div> */}
 
 						<div className="relative mt-6 flex items-center gap-x-4">
-							<img src={post.author.imageUrl} className="h-10 w-10 rounded-full bg-gray-100" />
+							{/* <img src={post.author.imageUrl} className="h-10 w-10 rounded-full bg-gray-100" /> */}
 							<div className="text-sm leading-6">
 								<p className="font-semibold text-gray-900">
 									<a href={post.author.href}>
@@ -234,7 +232,7 @@ const Accounts = () => {
 										{post.author.name}
 									</a>
 								</p>
-								<p className="text-gray-600">{post.author.role}</p>
+								<p className="text-gray-600">{post?.author?.role}</p>
 							</div>
 						</div>
 					</div>
