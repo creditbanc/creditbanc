a099a15448429e7b78b3f2eb1a1029aed5571aab
1infiniteloop <1infiniteloop.end@gmail.com>
2023-11-14 22:07:44 -0500

    .

--- app/routes/apply/1.$.jsx
+++ app/routes/apply/1.$.jsx
@@ -98,7 +98,7 @@ const LoansList = () => {
 
 const SectionHeading = ({ headline, subheadline }) => {
 	return (
-		<div className="flex flex-col text-center gap-y-3 my-7">
+		<div className="flex flex-col text-center gap-y-2 my-7">
 			<div className="text-gray-800 font-semibold">{headline}</div>
 			<div className="text-gray-600">{subheadline}</div>
 		</div>
@@ -282,13 +282,13 @@ export default function Container() {
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
-								stroke-width="1.5"
+								strokeWidth="1.5"
 								stroke="currentColor"
 								class="w-4 h-4"
 							>
 								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
+									strokeLinecap="round"
+									strokeLinejoin="round"
 									d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
 								/>
 							</svg>
