e35d17ddf6f0922f4bb6eea4102763f1b588ef7f
1infiniteloop <1infiniteloop.end@gmail.com>
2023-11-15 05:00:20 -0500

    .

---
+++ app/routes/apply/social.$.jsx
@@ -0,0 +1,124 @@
+import { classNames, mapIndexed } from "~/utils/helpers";
+import { pipe, map } from "ramda";
+import useStore from "./store";
+import { useState } from "react";
+import { RadioGroup } from "@headlessui/react";
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+
+const steps = [
+	{ id: "Financing Needs", name: "Job details", href: "#", status: "complete" },
+	{ id: "About your business", name: "Application form", href: "#", status: "current" },
+	{ id: "About your owners", name: "Preview", href: "#", status: "upcoming" },
+];
+
+const Progress = () => {
+	return (
+		<nav aria-label="Progress">
+			<ol role="list" className="space-y-4 md:flex md:space-x-8 md:space-y-0">
+				{pipe(
+					mapIndexed((step, index) => (
+						<li key={index} className="md:flex-1">
+							{step.status === "complete" ? (
+								<a
+									href={step.href}
+									className="group flex flex-col items-center border-l-4 border-blue-600 py-2 pl-4 hover:border-blue-800 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
+								>
+									<span className="text-sm font-medium text-blue-600 group-hover:text-blue-800">
+										{step.id}
+									</span>
+									{/* <span className="text-sm font-medium">{step.name}</span> */}
+								</a>
+							) : step.status === "current" ? (
+								<a
+									href={step.href}
+									className="flex flex-col items-center border-l-4 border-blue-600 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
+									aria-current="step"
+								>
+									<span className="text-sm font-medium text-blue-600">{step.id}</span>
+									{/* <span className="text-sm font-medium">{step.name}</span> */}
+								</a>
+							) : (
+								<a
+									href={step.href}
+									className="group flex flex-col items-center border-l-4 border-gray-200 py-2 pl-4 hover:border-gray-300 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
+								>
+									<span className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+										{step.id}
+									</span>
+									{/* <span className="text-sm font-medium">{step.name}</span> */}
+								</a>
+							)}
+						</li>
+					))
+				)(steps)}
+			</ol>
+		</nav>
+	);
+};
+
+const SectionHeading = ({ headline, subheadline }) => {
+	return (
+		<div className="flex flex-col text-center gap-y-2 my-7">
+			<div className="text-gray-800 font-semibold">{headline}</div>
+			<div className="text-gray-600">{subheadline}</div>
+		</div>
+	);
+};
+
+const OwnerForm = () => {
+	return (
+		<form>
+			<div className="space-y-12">
+				<div className="border-b border-gray-900/10 pb-12">
+					<div className="mt-10 flex flex-col">
+						<div className="flex flex-col w-full">
+							<label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+								Social
+							</label>
+							<div className="mt-2">
+								<input
+									type="text"
+									name="first-name"
+									id="first-name"
+									autoComplete="given-name"
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+	);
+};
+
+export default function Container() {
+	return (
+		<div className="flex flex-col items-center w-full h-full overflow-y-scroll pb-10">
+			<div className="flex flex-col w-full">
+				<Progress />
+			</div>
+			<div className="flex flex-col justify-center h-full">
+				<div className="flex flex-col my-4">
+					<SectionHeading
+						headline={<div>First, how soon do you need funding?</div>}
+						subheadline={
+							<div className="flex flex-row gap-x-2">
+								<div>This helps us find the best loan for your needs.</div>
+								<div className="font-semibold">Learn more ›</div>
+							</div>
+						}
+					/>
+				</div>
+				<div className="flex flex-col w-full">
+					<OwnerForm />
+				</div>
+				<div className="flex flex-col w-full items-center gap-y-4 my-5">
+					<div className="flex flex-col bg-blue-600 py-3 px-4 rounded-full text-white w-[450px] items-center cursor-pointer">
+						Continue to pre-qualify
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
