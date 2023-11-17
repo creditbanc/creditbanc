3395d28526ad48c316973e35c6e865516236ae95
1infiniteloop <1infiniteloop.end@gmail.com>
2023-11-17 02:04:18 -0500

    .

--- app/components/SimpleNavSignedIn.jsx
+++ app/components/SimpleNavSignedIn.jsx
@@ -638,7 +638,7 @@ const NotificationsDropdown = () => {
 				leaveFrom="transform opacity-100 scale-100"
 				leaveTo="transform opacity-0 scale-95"
 			>
-				<Menu.Items className="absolute right-0 z-10 mt-1 w-[500px] h-[450px] overflow-y-scroll origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+				<Menu.Items className="absolute right-0 z-10 mt-1 w-[500px] h-[450px] overflow-y-scroll origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none scrollbar-none">
 					<div className="sticky top-0">
 						<NotificationsHeading />
 					</div>
