9a2501d2a4cc496e480c69ba6676dfbc20d69777
1infiniteloop <1infiniteloop.end@gmail.com>
2023-11-20 06:52:22 -0500

    .

--- app/routes/apply/owners.$.jsx
+++ app/routes/apply/owners.$.jsx
@@ -1,9 +1,11 @@
 import { classNames, get_entity_id, get_group_id, mapIndexed } from "~/utils/helpers";
-import { pipe, map } from "ramda";
+import { pipe, map, head, values } from "ramda";
 import useStore from "./store";
 import { useState } from "react";
 import { RadioGroup } from "@headlessui/react";
 import { Link, useLocation } from "@remix-run/react";
+import { filter } from "shades";
+import { v4 as uuidv4 } from "uuid";
 
 const steps = [
 	{ id: "Financing Needs", name: "Job details", href: "#", status: "complete" },
@@ -68,115 +70,110 @@ const SectionHeading = ({ headline, subheadline }) => {
 const OwnerForm = () => {
 	return (
 		<form>
-			<div className="space-y-12">
-				<div className="border-b border-gray-900/10 pb-12">
-					<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-						<div className="sm:col-span-3">
-							<label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-								First name
-							</label>
-							<div className="mt-2">
-								<input
-									type="text"
-									name="first-name"
-									id="first-name"
-									autoComplete="given-name"
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
+			<div className="border-b border-gray-900/10 pb-12">
+				<div className=" grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+					<div className="sm:col-span-3">
+						<label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+							First name
+						</label>
+						<div className="mt-2">
+							<input
+								type="text"
+								name="first-name"
+								id="first-name"
+								autoComplete="given-name"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
 						</div>
+					</div>
 
-						<div className="sm:col-span-3">
-							<label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-								Last name
-							</label>
-							<div className="mt-2">
-								<input
-									type="text"
-									name="last-name"
-									id="last-name"
-									autoComplete="family-name"
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
+					<div className="sm:col-span-3">
+						<label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+							Last name
+						</label>
+						<div className="mt-2">
+							<input
+								type="text"
+								name="last-name"
+								id="last-name"
+								autoComplete="family-name"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
 						</div>
+					</div>
 
-						<div className="sm:col-span-4">
-							<label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-								Email address
-							</label>
-							<div className="mt-2">
-								<input
-									id="email"
-									name="email"
-									type="email"
-									autoComplete="email"
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
+					<div className="col-span-full">
+						<label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+							Email address
+						</label>
+						<div className="mt-2">
+							<input
+								id="email"
+								name="email"
+								type="email"
+								autoComplete="email"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
 						</div>
+					</div>
 
-						<div className="col-span-full">
-							<label
-								htmlFor="street-address"
-								className="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Street address
-							</label>
-							<div className="mt-2">
-								<input
-									type="text"
-									name="street-address"
-									id="street-address"
-									autoComplete="street-address"
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
+					<div className="col-span-full">
+						<label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
+							Street address
+						</label>
+						<div className="mt-2">
+							<input
+								type="text"
+								name="street-address"
+								id="street-address"
+								autoComplete="street-address"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
 						</div>
+					</div>
 
-						<div className="sm:col-span-2 sm:col-start-1">
-							<label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
-								City
-							</label>
-							<div className="mt-2">
-								<input
-									type="text"
-									name="city"
-									id="city"
-									autoComplete="address-level2"
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
+					<div className="sm:col-span-2 sm:col-start-1">
+						<label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
+							City
+						</label>
+						<div className="mt-2">
+							<input
+								type="text"
+								name="city"
+								id="city"
+								autoComplete="address-level2"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
 						</div>
+					</div>
 
-						<div className="sm:col-span-2">
-							<label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
-								State / Province
-							</label>
-							<div className="mt-2">
-								<input
-									type="text"
-									name="region"
-									id="region"
-									autoComplete="address-level1"
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
+					<div className="sm:col-span-2">
+						<label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
+							State / Province
+						</label>
+						<div className="mt-2">
+							<input
+								type="text"
+								name="region"
+								id="region"
+								autoComplete="address-level1"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
 						</div>
+					</div>
 
-						<div className="sm:col-span-2">
-							<label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
-								ZIP / Postal code
-							</label>
-							<div className="mt-2">
-								<input
-									type="text"
-									name="postal-code"
-									id="postal-code"
-									autoComplete="postal-code"
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
+					<div className="sm:col-span-2">
+						<label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
+							ZIP / Postal code
+						</label>
+						<div className="mt-2">
+							<input
+								type="text"
+								name="postal-code"
+								id="postal-code"
+								autoComplete="postal-code"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
 						</div>
 					</div>
 				</div>
@@ -188,6 +185,18 @@ const OwnerForm = () => {
 const owners = [
 	{
 		name: "Person",
+		type: "person",
+		model: {
+			first_name: "",
+			last_name: "",
+			email: "",
+			address: {
+				street: "",
+				city: "",
+				state: "",
+				zip: "",
+			},
+		},
 	},
 	{
 		name: "Business",
@@ -196,51 +205,83 @@ const owners = [
 
 const Owners = () => {
 	const [selected, setSelected] = useState(owners[0]);
+	const { set_path, owners: business_owners } = useStore();
+
+	const onAddOwner = (type) => {
+		console.log("onAddOwner");
+
+		let owner = pipe(filter({ type }), head)(owners);
+		let owner_id = uuidv4();
+
+		let payload = {
+			id: owner_id,
+			type,
+			...owner,
+		};
+
+		set_path(["owners", owner_id], payload);
+
+		console.log("owner");
+		console.log(owner);
+	};
 
 	return (
-		<RadioGroup value={selected} onChange={setSelected}>
-			<RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
-			<div className="space-y-4">
+		<div className="flex flex-col w-full">
+			<div className="flex flex-col w-full">
 				{pipe(
-					mapIndexed((plan, index) => (
-						<RadioGroup.Option
-							key={index}
-							value={plan}
-							className={({ active }) =>
-								classNames(
-									active ? "border-blue-600 ring-2 ring-blue-600" : "border-gray-300",
-									"relative block cursor-pointer rounded-full border bg-white px-6 py-4 shadow-sm focus:outline-none sm:flex sm:justify-between"
-								)
-							}
-						>
-							{({ active, checked }) => (
-								<>
-									<div className="flex items-center w-full">
-										<div className="flex flex-col text-sm items-center w-full">
-											<RadioGroup.Label
-												as="span"
-												className="flex flex-col items-center text-align center w-full font-medium text-gray-900"
-											>
-												{plan.name}
-											</RadioGroup.Label>
-										</div>
-									</div>
-
-									<div
-										className={classNames(
-											active ? "border" : "border-2",
-											checked ? "border-blue-600" : "border-transparent",
-											"pointer-events-none absolute -inset-px rounded-full"
-										)}
-										aria-hidden="true"
-									/>
-								</>
-							)}
-						</RadioGroup.Option>
+					values,
+					map((owner) => (
+						<div>
+							<OwnerForm owner={owner} />
+						</div>
 					))
-				)(owners)}
+				)(business_owners)}
 			</div>
-		</RadioGroup>
+			<RadioGroup value={selected} onChange={setSelected}>
+				<RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
+				<div className="space-y-4 my-4">
+					{business_owners.length > 0 && (
+						<div className="font-semibold flex flex-col items-center py-4">Add another owner</div>
+					)}
+					{pipe(
+						mapIndexed((plan, index) => (
+							<RadioGroup.Option
+								key={index}
+								value={plan}
+								className={({ active }) =>
+									classNames(
+										"relative block cursor-pointer rounded-full border bg-white px-6 py-4 shadow-sm focus:outline-none sm:flex sm:justify-between border-gray-300"
+									)
+								}
+								onClick={() => onAddOwner(plan.type)}
+							>
+								{({ active, checked }) => (
+									<>
+										<div className="flex items-center w-full">
+											<div className="flex flex-col text-sm items-center w-full">
+												<RadioGroup.Label
+													as="span"
+													className="flex flex-col items-center text-align center w-full font-medium text-gray-900"
+												>
+													{plan.name}
+												</RadioGroup.Label>
+											</div>
+										</div>
+
+										<div
+											className={classNames(
+												"pointer-events-none absolute -inset-px rounded-full border-transparent border-2"
+											)}
+											aria-hidden="true"
+										/>
+									</>
+								)}
+							</RadioGroup.Option>
+						))
+					)(owners)}
+				</div>
+			</RadioGroup>
+		</div>
 	);
 };
 
@@ -251,11 +292,11 @@ export default function Container() {
 
 	return (
 		<div className="flex flex-col items-center w-full h-full overflow-y-scroll pb-10">
-			<div className="flex flex-col w-full">
+			<div className="flex flex-col w-full mb-10">
 				<Progress />
 			</div>
-			<div className="flex flex-col justify-center h-4/5 w-[600px]">
-				<div className="flex flex-col my-4">
+			<div className="flex flex-col justify-center h-full w-[600px] my-[100px]">
+				<div className="flex flex-col">
 					<SectionHeading
 						headline={<div>Who owns 20% or more of roas?</div>}
 						subheadline={
@@ -264,12 +305,11 @@ export default function Container() {
 									Enter all owners of the business who own 20% or more of roas. Rest easy, this won’t
 									impact your credit and won’t show up as an inquiry on your credit report.
 								</div>
-								{/* <div className="font-semibold">Learn more ›</div> */}
 							</div>
 						}
 					/>
 				</div>
-				<div className="flex flex-col w-full">
+				<div className="flex flex-col w-full ">
 					<Owners />
 				</div>
 				<div className="flex flex-row w-full items-center gap-y-4 my-5 gap-x-3">
