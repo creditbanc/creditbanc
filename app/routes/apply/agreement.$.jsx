34204febcd7baecd6817985ec5c9ed303f392bcf
1infiniteloop <1infiniteloop.end@gmail.com>
2023-11-28 06:27:34 -0500

    .

--- app/routes/apply/agreement.$.jsx
+++ app/routes/apply/agreement.$.jsx
@@ -34,88 +34,99 @@ export default function Agreement() {
 	return (
 		<div className="flex flex-col items-center w-full h-full overflow-y-scroll absolute">
 			<div className="flex flex-col w-full">
-				<Progress />
+				<brogress />
 			</div>
 			<div className="flex flex-col w-full  scrollbar-none items-center relative pt-10">
 				<div className="flex flex-col h-fit items-center max-w-5xl py-10 absolute ">
-					<div className="flex flex-col">
-						<p>
-							THIS AGREEMENT, made this day of _____________, by and between SAG Advisors LLC having its
-							main office at 9315 Trinana Circle, Winter Garden, Florida (heretofore the Advisor); and
-							whose main business address is (heretofore the Client).
-						</p>
-						<p>
-							WHEREAS, the Advisor is hereby engaged to arrange financing or cause financing to be
-							provided to the client, via a Third-Party Lender. The Client has engaged the Advisor to
-							arrange financing for the Client. While procuring this financing, it became apparent Client
-							required a global consultancy service to facilitate the underwriting of said financing. This
-							consultancy includes, but is not limited to: financial analysis of bookkeeping and records,
-							review of business tax returns, payroll audits, review of any pending litigation and
-							corporate structures, These services may be provided by Advisor or by third party
-							professionals that Advisor may procure for the purposes of providing consultancy.
-						</p>
-						<p>NOW, THEREFORE IT IS AGREED AS FOLLOWS;</p>
-						<p>
-							The Client retains the services of the Advisor and appoints the Advisor to act as the
-							Client's Agent in all negotiations with potential Lenders introduced to the Client by the
-							Advisor.
-						</p>
-						<p>
-							The Client agrees to pay the Advisor a Success Fee (The Success Fee). The Success Fee shall
-							be equal to 5% Five Percent) of funding or Line obtained. The Success Fee will be made
-							payable to SAG Advisors LLC, via Certified Bank Check, ACH or via Wire Transfer (below
-							instruction), if the Lender does not do so on your behalf. You further authorize the lender
-							or closing agent to SAG Advisors LLC directly upon closing. By signing this agreement, you
-							unconditionally agree to this Success Fee and to pay such SAG Advisors LLC within 3 business
-							days of funding.
-						</p>
+					<div className="flex flex-col gap-y-5">
 						<div>
-							<p>Chase Bank</p>
-							<p>ABA # 021000021</p>
-							<p>Acct # 661261203</p>
-							<p>Payable: SAG Advisors LLC</p>
+							<b>THIS AGREEMENT</b>, made this day of _____________, by and between{" "}
+							<b>SAG Advisors LLC</b> having its main office at 9315 Trinana Circle, Winter Garden,
+							Florida (heretofore the <b>Advisor</b>); and whose main business address is (heretofore the{" "}
+							<b>Client</b> ).
+						</div>
+						<div>
+							<b>WHEREAS</b>, the
+							<b>Advisor</b>
+							is hereby engaged to arrange financing or cause financing to be provided to the client, via
+							a Third-Party Lender. The <b>Client</b> has engaged the <b>Advisor</b>
+							to arrange financing for the <b>Client</b>. While procuring this financing, it became
+							apparent
+							<b>Client</b> required a global consultancy service to facilitate the underwriting of said
+							financing. This consultancy includes, but is not limited to: financial analysis of
+							bookkeeping and records, review of business tax returns, payroll audits, review of any
+							pending litigation and corporate structures, These services may be provided by{" "}
+							<b>Advisor</b> or by third party professionals that <b>Advisor</b> may procure for the
+							purposes of providing consultancy.
+						</div>
+						<div>
+							<b>NOW, THEREFORE IT IS AGREED AS FOLLOWS;</b>
+						</div>
+						<div>
+							The <b>Client</b> retains the services of the <b>Advisor</b> and appoints the <b>Advisor</b>{" "}
+							to act as the Client's Agent in all negotiations with potential Lenders introduced to the
+							<b>Client</b> by the <b>Advisor</b>.
+						</div>
+						<div>
+							The <b>Client</b> agrees to pay the <b>Advisor</b> a <b>Success Fee (The Success Fee)</b>.{" "}
+							<b>The Success Fee</b> shall be equal to <b>5% Five Percent</b> ) of funding or Line
+							obtained. The <b>Success Fee</b> will be made payable to <b>SAG Advisors LLC</b>, via
+							Certified Bank Check, ACH or via Wire Transfer (below instruction), if the Lender does not
+							do so on your behalf. You further authorize the lender or closing agent to{" "}
+							<b>SAG Advisors LLC</b> directly upon closing. By signing this agreement, you
+							unconditionally agree to this <b>Success Fee</b> and to pay such <b>SAG Advisors LLC</b>
+							within <b>3</b> business days of funding.
+						</div>
+						<div>
+							<div>Chase Bank</div>
+							<div>ABA#: 021000021</div>
+							<div>Acct#: 661261203</div>
+							<div>Payable: SAG Advisors LLC</div>
 						</div>
 
-						<p>
-							IT IS UNDERSTOOD AND AGREED that the Advisor will have earned this fee or commission at the
-							time the service is rendered, and benefit rendered unto the Client.
-						</p>
-
-						<p>
-							Client agrees to defend the Advisor against any claim(s) made by any third- p a r t y
-							service providers, not affiliated with the Advisor, for any part of the Success Fee earned
-							by the Advisor as it relates to this transaction. Client gives the Advisor and any potential
-							lender the right to perform and all investigations necessary to evaluate the Client's credit
-							worthiness. The Client represents that all information submitted to the Advisor is
-							substantially true and accurate. However, should it be determined that the information
-							provided is materially false; the Success Fee delineated above shall become immediately due
-							and payable.
-						</p>
-
-						<p>
-							Client agrees that neither he/she nor any of their associated entities or agents will enter
-							into any transaction(s) with any Lender, introduced by the Advisor to the Client, for a
-							period of Tweleve (12) months from the date of said introduction or Closing, whichever
-							occurs last, without recognizing SAG Advisors LLC as the Advisor of Record and as such
-							entitled to a Success Fee. Unless renegotiated, via a new Brokerage Agreement, the fee(s)
-							set forward in this Agreement shall apply
-						</p>
-						<p>
-							The Client agrees to reimburse the Advisor for any and all reasonable costs incurred, by the
-							Advisor, in effectuating collection of said Success Fee and/or Fee(s), including but not
-							limited to, legal and court costs. Client will be responsible for said costs only if a
-							Florida court or arbiter finds the Client culpable.
-						</p>
-
-						<p>
-							Facsimile signatures will be deemed acceptable, as original, by all parties of this
-							Agreement. The Law's of the State of Florida shall govern this contract.
-						</p>
 						<div>
-							<p>By:</p>
-							<p>Name:</p>
-							<p>Title:</p>
-							<p>Date:</p>
+							IT IS UNDERSTOOD AND AGREED that the <b>Advisor</b> will have earned this fee or commission
+							at the time the service is rendered, and benefit rendered unto the <b>Client</b>.
+						</div>
+
+						<div>
+							<b>Client</b> agrees to defend the <b>Advisor</b> against any claim(s) made by any third-
+							party service providers, not affiliated with the <b>Advisor</b>, for any part of the
+							<b>Success Fee</b>
+							earned by the <b>Advisor</b> as it relates to this transaction. <b>Client</b> gives the{" "}
+							<b>Advisor</b> and any potential lender the right to perform and all investigations
+							necessary to evaluate the <b>Client</b>'s credit worthiness. The <b>Client</b> represents
+							that all information submitted to the <b>Advisor</b> is substantially true and accurate.
+							However, should it be determined that the information provided is materially false; the{" "}
+							<b>Success Fee</b>
+							delineated above shall become immediately due and payable.
+						</div>
+
+						<div>
+							<b>Client</b> agrees that neither he/she nor any of their associated entities or agents will
+							enter into any transaction(s) with any Lender, introduced by the <b>Advisor</b> to the{" "}
+							<b>Client</b>, for a period of <b>Tweleve (12)</b> months from the date of said introduction
+							or Closing, whichever occurs last, without recognizing <b>SAG Advisors LLC</b> as the{" "}
+							<b>Advisor</b> of Record and as such entitled to a <b>Success Fee</b>. Unless renegotiated,
+							via a new Brokerage Agreement, the fee(s) set forward in this Agreement shall apply
+						</div>
+						<div>
+							The <b>Client</b> agrees to reimburse the <b>Advisor</b> for any and all reasonable costs
+							incurred, by the <b>Advisor</b>, in effectuating collection of said <b>Success Fee</b>{" "}
+							and/or Fee(s), including but not limited to, legal and court costs. <b>Client</b> will be
+							responsible for said costs only if a Florida court or arbiter finds the <b>Client</b>{" "}
+							culpable.
+						</div>
+
+						<div>
+							Facsimile signatures will be deemed acceptable, as original, by all parties of this
+							Agreement. The Law's of the <b>State of Florida</b> shall govern this contract.
+						</div>
+						<div>
+							<div>By:</div>
+							<div>Name:</div>
+							<div>Title:</div>
+							<div>Date:</div>
 						</div>
 					</div>
 				</div>
