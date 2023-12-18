877958f8b7b5bd5aded82644cfd8a44244e60475
1infiniteloop <1infiniteloop.end@gmail.com>
2023-12-18 05:56:16 -0500

    .

--- app/routes/sms/send.$.jsx
+++ app/routes/sms/send.$.jsx
@@ -1,27 +1,27 @@
-import { form_params } from "~/utils/helpers";
+// import { form_params } from "~/utils/helpers";
 
-const client = require("twilio")(process.env.TWILLIO_ACCOUNT_SID, process.env.TWILLIO_AUTH_TOKEN);
+// const client = require("twilio")(process.env.TWILLIO_ACCOUNT_SID, process.env.TWILLIO_AUTH_TOKEN);
 
-export const action = async ({ request }) => {
-	let form = await form_params(request);
+// export const action = async ({ request }) => {
+// 	let form = await form_params(request);
 
-	console.log("emailform");
-	console.log(form);
+// 	console.log("emailform");
+// 	console.log(form);
 
-	let { payload } = form;
-	let { group_id, entity_id, to, from } = JSON.parse(payload);
+// 	let { payload } = form;
+// 	let { group_id, entity_id, to, from } = JSON.parse(payload);
 
-	try {
-		client.messages
-			.create({
-				body: "A new application has been submitted.",
-				to,
-				from: "3526448623",
-			})
-			.then((message) => console.log(message.sid));
-	} catch (error) {
-		console.log(error);
-	}
+// 	try {
+// 		client.messages
+// 			.create({
+// 				body: "A new application has been submitted.",
+// 				to,
+// 				from: "3526448623",
+// 			})
+// 			.then((message) => console.log(message.sid));
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
 
-	return null;
-};
+// 	return null;
+// };
